063e01ad56c65c7714b43a5d4c98a349b8498617
Mehran Eimanlou <mehran.eimanlou@gmail.com>
2021-11-22 12:51:05 -0500

    docs: Changes to documentation; no code change Added comments: project description to main code. No code change

--- Project_bikeshare.py
+++ Project_bikeshare.py
@@ -4,6 +4,24 @@ import numpy as np
 import calendar
 import datetime
 
+"""
+### Project Description
+Used to view and analyse bikeshare data from distinct cities based on user input.
+
+The program let's user to filter bikeshare data based on city, month, and day of the week:
+
+- The following city data are accessible to the user:
+  1. **chicago**
+  2. **washington**
+  3. **new_york_city**
+
+- Following features can be accessed:
+  1. **Statistics of the most common month, day, hour of travel**
+  2. **Statistics of the most common start, end, and the route of travel**
+  3. **Statistics of the total travel time**
+  4. **Statistics of users gender and the year of birth**
+  5. **Display the header of the selected data**
+  """
 
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
